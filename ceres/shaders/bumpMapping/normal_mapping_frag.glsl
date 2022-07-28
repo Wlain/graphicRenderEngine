65e3c5abe0ec418e2965119d421f607da54eec5a
william <1049758437@qq.com>
2022-07-28 11:02:53 +0800

    update

--- ceres/shaders/bumpMapping/normal_mapping_frag.glsl
+++ ceres/shaders/bumpMapping/normal_mapping_frag.glsl
@@ -2,24 +2,11 @@
 out vec4 fragColor;
 in vec3 vNormal;
 in vec2 vUv;
+in vec3 vWorldPos;
 
 #include "utils_incl.glsl"
 uniform sampler2D normalTex;
 uniform sampler2D tex;
-
-mat3 calculateTBN()
-{
-    vec3 pos_dx = dFdx(vWsPos);
-    vec3 pos_dy = dFdy(vWsPos);
-    vec3 tex_dx = dFdx(vec3(vUV, 0.0));
-    vec3 tex_dy = dFdy(vec3(vUV, 0.0));
-    vec3 t = (tex_dy.t * pos_dx - tex_dx.t * pos_dy) / (tex_dx.s * tex_dy.t - tex_dy.s * tex_dx.t);
-    vec3 ng = normalize(vNormal);
-    t = normalize(t - ng * dot(ng, t));
-    vec3 b = normalize(cross(ng, t));
-    mat3 tbn = mat3(t, b, ng);
-}
-
 
 void main()
 {
